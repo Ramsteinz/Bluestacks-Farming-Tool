bc7f772b1ab74ca944f21d4cea661b34fe4b5fbb
jkralicky <cobalt77@gmx.com>
2015-02-18 14:14:09 -0500

    Initial Commit

---
+++ Project Files/BlueStacks/BlueStacks/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace BlueStacks {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing ) {
+			if ( disposing && ( components != null ) ) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 114);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(167, 84);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 98);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Image Analysis";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(223, 126);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(223, 166);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(185, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Gold:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(185, 169);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(31, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Elixir:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(12, 47);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 7;
+			this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(127, 47);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 8;
+			this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(9, 28);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(73, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Minimum Gold";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(124, 28);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(72, 13);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Minimum Elixir";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(250, 50);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(73, 17);
+			this.checkBox1.TabIndex = 11;
+			this.checkBox1.Text = "Auto Next";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(13, 74);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(0, 13);
+			this.label6.TabIndex = 12;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(13, 224);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(97, 13);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "Advanced Settings";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(15, 253);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(251, 13);
+			this.label8.TabIndex = 14;
+			this.label8.Text = "Distance from top left corner to search for resources";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(18, 278);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(14, 13);
+			this.label9.TabIndex = 15;
+			this.label9.Text = "X";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(124, 278);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(14, 13);
+			this.label10.TabIndex = 16;
+			this.label10.Text = "Y";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(38, 275);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(52, 20);
+			this.textBox5.TabIndex = 17;
+			this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(144, 275);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(52, 20);
+			this.textBox6.TabIndex = 18;
+			this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(13, 310);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(121, 13);
+			this.label11.TabIndex = 19;
+			this.label11.Text = "Size of search container";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(144, 335);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(52, 20);
+			this.textBox7.TabIndex = 23;
+			this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(38, 335);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(52, 20);
+			this.textBox8.TabIndex = 22;
+			this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(124, 338);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(14, 13);
+			this.label12.TabIndex = 21;
+			this.label12.Text = "Y";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(18, 338);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(14, 13);
+			this.label13.TabIndex = 20;
+			this.label13.Text = "X";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(15, 368);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(245, 13);
+			this.label14.TabIndex = 24;
+			this.label14.Text = "Distance from bottom right corner to hit next button";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(144, 392);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(52, 20);
+			this.textBox9.TabIndex = 28;
+			this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(38, 392);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.Size = new System.Drawing.Size(52, 20);
+			this.textBox10.TabIndex = 27;
+			this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Location = new System.Drawing.Point(124, 395);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(14, 13);
+			this.label15.TabIndex = 26;
+			this.label15.Text = "Y";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Location = new System.Drawing.Point(18, 395);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(14, 13);
+			this.label16.TabIndex = 25;
+			this.label16.Text = "X";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(248, 300);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 29;
+			this.button1.Text = "Save All";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(346, 424);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.textBox10);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "BlueStacks Farming Tool";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Button button1;
+
+	}
+}
+
---
+++ Project Files/BlueStacks/BlueStacks/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using Tesseract;
+using Timer = System.Timers.Timer;
+using C = System.Convert;
+
+namespace BlueStacks {
+	public partial class Form1 : Form {
+		private string x1_add;
+		private string y1_add;
+		private string len;
+		private string height;
+		private string next_x;
+		private string next_y;
+		const string defaultContents =
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<config>
+	<setting key=""x1_add"" value=""58"" />
+	<setting key=""y1_add"" value=""82"" />
+	<setting key=""len"" value=""115"" />
+	<setting key=""height"" value=""70"" />
+	<setting key=""next_x"" value=""150"" />
+	<setting key=""next_y"" value=""300"" />
+</config>";
+		public Form1() {
+			InitializeComponent();
+			if ( !File.Exists( "./settings.xml" ) ) {
+				using ( var sw = new StreamWriter( "./settings.xml" ) ) {
+					sw.Write( defaultContents );
+				}
+			}
+			Settings.Initialize();
+			textBox5.Text = x1_add = Settings.Get( "x1_add" );
+			textBox6.Text = y1_add = Settings.Get( "y1_add" );
+			textBox8.Text = len = Settings.Get( "len" );
+			textBox7.Text = height = Settings.Get( "height" );
+			textBox10.Text = next_x = Settings.Get( "next_x" );
+			textBox9.Text = next_y = Settings.Get( "next_y" );
+			var t = new Thread( () => {
+				var t1 = new Timer( 500 );
+				t1.Elapsed += delegate {
+					if ( Util.GetActiveWindowTitle() == "BlueStacks App Player" ) {
+						Util._RECT r;
+						Util.GetWindowRect( Util.GetActiveWindow(), out r );
+						int x1 = r.Left + 8;
+						int y1 = r.Top + 30;
+						var rect = new Rectangle( x1 + C.ToInt32( x1_add ), y1 + C.ToInt32( y1_add ), C.ToInt32( len ), C.ToInt32( height ) );
+						Bitmap b = new Bitmap( rect.Width, rect.Height );
+						Graphics g = Graphics.FromImage( b );
+						try {
+							g.CopyFromScreen( rect.Left, rect.Top, 0, 0, b.Size, CopyPixelOperation.SourceCopy );
+						} catch {
+						}
+						try {
+							for ( int i = 0; i <= rect.Width; ++i ) {
+								for ( int ii = 0; ii <= rect.Height; ++ii ) {
+									if ( i < rect.Width && ii < rect.Height ) {
+										Color c = b.GetPixel( i, ii );
+										if ( c != Util.Gold && c != Util.Elixir ) {
+											b.SetPixel( i, ii, Color.Black );
+										} else {
+											b.SetPixel( i, ii, Color.White );
+										}
+									}
+								}
+							}
+						} catch {
+						}
+						try {
+							pictureBox1.Image = b;
+						} catch {
+						}
+						try {
+							using ( var eng = new TesseractEngine( Directory.GetCurrentDirectory() + @".\tessdata", "eng", EngineMode.Default ) ) {
+								eng.SetVariable( "tessedit_char_whitelist", "1234567890" );
+								label6.Invoke( new Action( delegate {
+									label6.Text = String.Empty;
+								} ) );
+								try {
+									using ( var img = PixConverter.ToPix( b ) ) {
+										using ( var txt = eng.Process( img ) ) {
+											var text = txt.GetText();
+											string[] spl = text.Split( '\n' );
+											textBox1.Invoke( new Action( delegate {
+												try {
+													textBox1.Text = spl[ 0 ].Replace( " ", String.Empty );
+													textBox2.Text = spl[ 1 ].Replace( " ", String.Empty );
+												} catch ( IndexOutOfRangeException ) {
+												}
+												int minG = 0;
+												int minE = 0;
+												int curG = 0;
+												int curE = 0;
+												try {
+													minG = Convert.ToInt32( textBox3.Text );
+												} catch {
+												}
+												try {
+													minE = Convert.ToInt32( textBox4.Text );
+												} catch {
+												}
+												try {
+													curG = Convert.ToInt32( spl[ 0 ].Replace( " ", String.Empty ) );
+												} catch {
+												}
+												try {
+													curE = Convert.ToInt32( spl[ 1 ].Replace( " ", String.Empty ) );
+												} catch {
+												}
+												if ( curG >= minG || curE >= minE ) {
+													checkBox1.Invoke( new Action( delegate {
+														checkBox1.Checked = false;
+													} ) );
+												} else {
+													if ( checkBox1.Checked ) {
+														if ( ( textBox3.Text.Length > 0 || textBox4.Text.Length > 0 ) && ( curE != 0 || curG != 0 ) ) {
+															Util.LeftMouseClick( r.Right - C.ToInt32( next_x ), r.Bottom - C.ToInt32( next_y ) );
+														}
+													}
+												}
+											} ) );
+										}
+									}
+								} catch {
+								}
+							}
+						} catch ( TesseractException ) {
+							label6.Invoke( new Action( delegate {
+								label6.Text = "Cannot Initialize Tesseract Engine";
+							} ) );
+						}
+						g.Dispose();
+					}
+				};
+				t1.Start();
+			} );
+			t.Start();
+		}
+
+		private void textBox3_KeyPress( object sender, KeyPressEventArgs e ) {
+			if ( !char.IsControl( e.KeyChar ) && !char.IsDigit( e.KeyChar ) && ( e.KeyChar != '.' ) ) {
+				e.Handled = true;
+			}
+		}
+
+		private void textBox4_KeyPress( object sender, KeyPressEventArgs e ) {
+			if ( !char.IsControl( e.KeyChar ) && !char.IsDigit( e.KeyChar ) && ( e.KeyChar != '.' ) ) {
+				e.Handled = true;
+			}
+		}
+
+		private void textBox5_TextChanged( object sender, EventArgs e ) {
+			x1_add = textBox5.Text;
+		}
+
+		private void textBox6_TextChanged( object sender, EventArgs e ) {
+			y1_add = textBox6.Text;
+		}
+
+		private void textBox8_TextChanged( object sender, EventArgs e ) {
+			len = textBox8.Text;
+		}
+
+		private void textBox7_TextChanged( object sender, EventArgs e ) {
+			height = textBox7.Text;
+		}
+
+		private void textBox10_TextChanged( object sender, EventArgs e ) {
+			next_x = textBox10.Text;
+		}
+
+		private void textBox9_TextChanged( object sender, EventArgs e ) {
+			next_y = textBox5.Text;
+		}
+
+		private void button1_Click( object sender, EventArgs e ) {
+			Settings.Set( "x1_add", x1_add );
+			Settings.Set( "y1_add", y1_add );
+			Settings.Set( "len", len );
+			Settings.Set( "height", height );
+			Settings.Set( "next_x", next_x );
+			Settings.Set( "next_y", next_y );
+		}
+	}
+
+	public class Util {
+		public static Color Gold = Color.FromArgb( 255, 255, 251, 204 );
+		public static Color Elixir = Color.FromArgb( 255, 255, 232, 253 );
+
+		[DllImport( "user32.dll" )]
+		private static extern bool SetCursorPos( int x, int y );
+
+		[DllImport( "user32.dll" )]
+		public static extern void mouse_event( int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo );
+
+		public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+		public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+		public static void LeftMouseClick( int xpos, int ypos ) {
+			SetCursorPos( xpos, ypos );
+			mouse_event( MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0 );
+			mouse_event( MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0 );
+		}
+
+		[DllImport( "user32.dll" )]
+		private static extern IntPtr GetDC( IntPtr hwnd );
+
+		[DllImport( "user32.dll" )]
+		private static extern Int32 ReleaseDC( IntPtr hwnd, IntPtr hdc );
+
+		[DllImport( "gdi32.dll" )]
+		private static extern uint GetPixel( IntPtr hdc, int nXPos, int nYPos );
+
+		[DllImport( "user32.dll" )]
+		private static extern IntPtr GetForegroundWindow();
+
+		[DllImport( "user32.dll" )]
+		private static extern int GetWindowText( IntPtr hWnd, StringBuilder text, int count );
+
+		[DllImport( "user32.dll" )]
+		[return: MarshalAs( UnmanagedType.Bool )]
+		public static extern bool GetWindowRect( IntPtr hWnd, out _RECT lpRect );
+
+		public static IntPtr GetActiveWindow() {
+			return GetForegroundWindow();
+		}
+
+		[StructLayout( LayoutKind.Sequential )]
+		public struct _RECT {
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+		}
+
+		public static Color GetPixelColor( int x, int y ) {
+			IntPtr hdc = GetDC( IntPtr.Zero );
+			uint pixel = GetPixel( hdc, x, y );
+			ReleaseDC( IntPtr.Zero, hdc );
+			int one = 0x000000FF;
+			int two = 0x0000FF00;
+			int three = 0x00FF0000;
+			Color color = Color.FromArgb( ( int ) ( pixel & one ), ( int ) ( pixel & two ) >> 8, ( int ) ( pixel & three ) >> 16 );
+			return color;
+		}
+
+		public static string GetActiveWindowTitle() {
+			const int nChars = 256;
+			StringBuilder Buff = new StringBuilder( nChars );
+			IntPtr handle = GetForegroundWindow();
+
+			if ( GetWindowText( handle, Buff, nChars ) > 0 ) {
+				return Buff.ToString();
+			}
+			return null;
+		}
+	}
+	public static class Settings {
+		private static XmlDocument doc;
+		private const string settingsfile = "./settings.xml";
+		public static void Initialize() {
+			doc = new XmlDocument();
+			doc.Load( settingsfile );
+		}
+		public static string Get( string key, string def = "" ) {
+			XmlNode node = doc.SelectSingleNode( "config/setting[@key='" + key + "']" );
+			if ( node == null ) {
+				return def;
+			}
+			return node.Attributes[ "value" ].Value ?? def;
+		}
+		public static void Set( string key, string value ) {
+			XmlNode node = doc.SelectSingleNode( "config/setting[@key='" + key + "']" );
+			if ( node != null ) {
+				node.Attributes[ "value" ].Value = value;
+				doc.Save( settingsfile );
+			}
+		}
+	}
+}
